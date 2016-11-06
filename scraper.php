5f2160181b0fcf5bb9de06551fe0c49436bf6bfc
pmoorcraft <pedro.moorcraft@gmail.com>
2016-11-06 17:38:45 +0100

    new action hook on activation

--- scraper.php
+++ scraper.php
@@ -13,6 +13,7 @@ Author URI: http://instacraft.io
 */
 
 
+
 /*
 * On plugin activation, the function below will schedule activate of the create posts function
 */
@@ -25,37 +26,25 @@ register_activation_hook( __FILE__, 'create_posts_function' );
 
 function create_posts_function (){
 
- if( $xml = simplexml_load_file("http://api.indeed.com/ads/apisearch?publisher=4425239534329302&q=%22%22&sort=&radius=&st=&jt=&start=&limit=25&fromage=&filter=&latlong=1&co=id&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2"))
+ if( $xml = simplexml_load_file("http://api.indeed.com/ads/apisearch?publisher=4425239534329302&q=%22%22&sort=&radius=&st=&jt=&start=&limit=25&fromage=&filter=&latlong=1&co=pt&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2"))
   {
     foreach($xml->results->result as $detail)
     {
-      $camp_id = $detail->jobkey;
-      $camp_name = $detail->company;
+      $category = get_category_by_slug( 'Local Jobs' );
+      $job_id = $detail->jobkey;
+      $job_name = $detail->jobtitle;
+      $job_link = $detail->url;
       $my_post = array(
-        'post_title'    => $detail->company,
-        'post_content'  => $detail->snippet,
+        'post_title'    => $detail->jobtitle,
+        'post_content'  => $detail->snippet."<a href =$job_link > Apply Now! </a>",
+        'post_category' => array( $category->term_id ),
         'post_status'   => 'draft',
         'post_author'   => 1,
       );
 
-      $query_post = array(
-        'post_tite'    => (string)$detail->company,
-        'post_status'   => 'draft',
-      );
+        if ( post_exists($job_name)) {
 
-
-      $my_query = new WP_Query( $query_post );
-
-      if ( $my_query->have_posts() ) {
-
-      }
-      //$query = $wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE post_title = %s AND (post_status = 'draft' OR post_status = 'publish')", $camp_name
-      //);
-
-        //$wpdb->query( $query );
-
-        //if ( $wpdb->num_rows > 0) {
-        //}
+        }
         else {
         wp_insert_post( $my_post );
 
